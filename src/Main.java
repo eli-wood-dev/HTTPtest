2a76c0d36dd131ae18e9dccd671598e15efbc0b6
Eli Wood <75440887+ew20kt@users.noreply.github.com>
2023-04-17 20:45:35 -0400

    it works

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,14 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.http.*;
 import java.util.Locale;
-import com.fasterxml.jackson.*;
+//import com.fasterxml.jackson.*;
 
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
 
-        HttpRequest request = HttpRequest.newBuilder(URI.create("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"))
-            //.header("accept", "application/json")
-            .build();
+        HttpRequest request = HttpRequest.newBuilder(URI.create("https://ow-api.com/v1/stats/pc/us/ThatGuy-15264/profile")).build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.statusCode());
