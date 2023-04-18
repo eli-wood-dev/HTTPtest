1296fc765c7847fea93da55a4c2848990a49674b
Eli Wood <75440887+ew20kt@users.noreply.github.com>
2023-04-17 20:05:59 -0400

    it works

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.*;
+import java.util.Locale;
+import com.fasterxml.jackson.*;
 
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder(URI.create("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"))
+            //.header("accept", "application/json")
+            .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
     }
 }
